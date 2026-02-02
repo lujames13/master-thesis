e070e01c01aa06367417cf0bcae37d468ad9e683
lujames13 <lujames13@gmail.com>
2026-02-02 20:56:42 +0800

    fix broken pdf

--- latex-thesis/build.sh
+++ latex-thesis/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-xelatex main
-biber main
-xelatex main
-xelatex main
+# 使用 latexmk 自動處理編譯次數與 biber
+# -xelatex: 使用 xelatex 編譯
+# -interaction=nonstopmode: 遇到錯誤不卡住
+# -f: 強制編譯
+latexmk -xelatex -interaction=nonstopmode main
